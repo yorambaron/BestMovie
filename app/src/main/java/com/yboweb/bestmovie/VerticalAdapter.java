16659211f9cd3199afee2aa8f414721e8c519c88
yorambaron <yoram@bar-on.org>
2016-06-26 16:58:29 +0300

    Change package URL

--- app/src/main/java/com/yboweb/bestmovie/VerticalAdapter.java
+++ app/src/main/java/com/yboweb/bestmovie/VerticalAdapter.java
@@ -10,7 +10,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.javacodegeeks.androidnavigationdrawerexample.R;
+import com.yboweb.bestmovie.androidnavigationdrawerexample.R;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
